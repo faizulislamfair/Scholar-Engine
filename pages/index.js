20e39641b39aaf0053bb73edf73fcb0dc2fadb4e
faizulislamfair <faizulislamfair@gmail.com>
2024-02-08 01:05:26 +0600

    updated

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,20 @@ export default function Home() {
 
       <p className='text-center text-lg p-5'>Scholar Engine</p>
 
-      <div className='text-center text-5xl p-5'>
-        Unlock Your Dream of <br />
-        Academic Excellence with <br />
-        Tailored Pathways
+      <div className='text-center text-5xl p-5 font-bold'>
+        <div className=''>
+          <img className='w-[250px] ml-20 translate-x-[300px] translate-y-[25px]' src="/airplane.svg" alt="Airplane" />
+          Unlock Your Dream of
+        </div>
+
+        <div>
+          Academic Excellence with <img className='w-[100px] translate-x-[995px] -translate-y-[90px]' src="/hat.svg" alt="Hat" />
+        </div>
+
+        <div className='-translate-y-[90px]'>
+          Tailored Pathways
+        </div>
+
       </div>
 
 
